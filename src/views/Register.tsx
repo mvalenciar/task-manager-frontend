702ba0cd733698998c9f5d25d8b7fac5ce5871e5
Milton Valencia R <miltonalonsovalenciarincon@gmail.com>
2026-07-06 17:13:46 -0500

    feat: implementar vista de registro de usuarios utilizando shadcn ui y tailwind css

---
+++ src/views/Register.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const Register = () => {
+	const [Email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		console.log(Email, password);
+	};
+
+	return (
+		<div>
+			<Card className="w-full max-w-sm mx-auto my-5">
+				<CardHeader>
+					<CardTitle>Crea tu cuenta</CardTitle>
+					<CardDescription>
+						Ingresa tu email y contraseña para crear una cuenta de usuario.
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<form onSubmit={handleSubmit}>
+						<div className="flex flex-col gap-6">
+							<div className="grid gap-2">
+								<Label htmlFor="email">Email</Label>
+								<Input
+									id="email"
+									type="email"
+									placeholder="email@example.com"
+									required
+									onChange={(e) => setEmail(e.target.value)}
+								/>
+							</div>
+							<div className="grid gap-2">
+								<div className="flex items-center">
+									<Label htmlFor="password">Contraseña</Label>
+									<a
+										href="https://example.com"
+										className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+									>
+										olvidaste tu contraseña?
+									</a>
+								</div>
+								<Input
+									id="password"
+									type="password"
+									required
+									onChange={(e) => setPassword(e.target.value)}
+								/>
+							</div>
+							<div className="grid gap-2">
+								<Button>Crear cuenta</Button>
+							</div>
+						</div>
+					</form>
+				</CardContent>
+			</Card>
+		</div>
+	);
+};
+
+export default Register;
