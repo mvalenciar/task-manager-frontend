c5de5c624f438fc6d33ddf6434f3cb5fe8a77858
Milton Valencia R <miltonalonsovalenciarincon@gmail.com>
2026-07-07 18:31:17 -0500

    feat: implementar rutas anidadas y proteger el acceso al Dashboard usando ProtectedRoute

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,21 @@
 import { Route, Routes } from "react-router-dom";
+
+import Dashboard from "@/views/Dashboard";
 import Login from "@/views/Login";
 import Register from "@/views/Register";
+import ProtectedRoute from "./ProtectedRoute";
 
 export const AppRoutes = () => {
 	return (
 		<Routes>
+			{/* 🔓 RUTAS PÚBLICAS */}
 			<Route path="/login" element={<Login />} />
 			<Route path="/register" element={<Register />} />
+
+			{/* 🔒 RUTAS PROTEGIDAS */}
+			<Route element={<ProtectedRoute />}>
+				<Route path="/dashboard" element={<Dashboard />} />
+			</Route>
 		</Routes>
 	);
 };
---
+++ src/views/Dashboard.tsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+	return (
+		<div>
+			<h1>📊 Bienvenido a tu Panel de Tareas Protegido</h1>
+		</div>
+	);
+};
+
+export default Dashboard;
