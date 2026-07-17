3cfb3ef9ab8d3714b90b1539f9cf27c346a061d0
Milton Valencia R <miltonalonsovalenciarincon@gmail.com>
2026-07-17 12:58:59 -0500

    test: implementar prueba unitaria para deleteTaskByApi validando la URL dinamica y cabeceras DELETE

--- src/tests/features/tasks/actions/delete-task-by-api.test.ts
+++ src/tests/features/tasks/actions/delete-task-by-api.test.ts
@@ -1,10 +1,36 @@
+import { describe, expect, test, vi } from "vitest";
+import { deleteTaskByApi } from "@/features/tasks/actions/delete-task-by-api";
 import { taskApi } from "@/services/api";
-import { describe, test, vi } from "vitest";
 
 taskApi;
 
-vi.mock("@/services/ap", () => ({
+vi.mock("@/services/api", () => ({
 	taskApi: {
 		delete: vi.fn(),
 	},
 }));
+
+describe("taskApi", () => {
+	test("should first", async () => {
+		localStorage.setItem("task_token", "test_token");
+
+		const taskId = 1;
+		const token = localStorage.getItem("task_token") as string;
+
+		vi.mocked(taskApi.delete).mockResolvedValue({
+			data: {
+				message: "✅ Tarea eliminada con éxito!",
+			},
+		});
+
+		const result = await deleteTaskByApi(taskId, token);
+
+		expect(result).toBe(true);
+		expect(taskApi.delete).toHaveBeenCalledTimes(1);
+		expect(taskApi.delete).toHaveBeenCalledWith(`/tasks/${taskId}`, {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
+	});
+});
