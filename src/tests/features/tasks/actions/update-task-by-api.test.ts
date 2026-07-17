f33e270983ff10011fce9cfa047e18245ce626f7
Milton Valencia R <miltonalonsovalenciarincon@gmail.com>
2026-07-17 17:38:35 -0500

    test: culminar suite de updateTaskByApi con escenario de error asincrono y sellar 100% de coverage en actions

--- src/tests/features/tasks/actions/update-task-by-api.test.ts
+++ src/tests/features/tasks/actions/update-task-by-api.test.ts
@@ -38,4 +38,19 @@ describe("update-task-by-api.test", () => {
 			},
 		);
 	});
+
+	test("should return false when updating task is fail", async () => {
+		localStorage.setItem("task_token", "test_token");
+		const taskId: number = 1;
+		const title: string = "updated_tittle";
+		const description: string = "updated_description";
+		const token: string = localStorage.getItem("task_token") as string;
+		const consoleSpy = vi.spyOn(console, "error").mockImplementation(() => {});
+
+		vi.mocked(taskApi.put).mockRejectedValue(new Error("Fail updating task"));
+
+		const result = await updateTaskByApi(taskId, title, description, token);
+
+		expect(result).toBe(false);
+	});
 });
