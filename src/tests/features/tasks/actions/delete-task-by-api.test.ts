b6d9fdb586d65013a42a5fbc895e64bf970527b2
Milton Valencia R <miltonalonsovalenciarincon@gmail.com>
2026-08-07 17:21:10 -0500

    test(tasks): corregir pruebas unitarias de la acción deleteTaskByApi eliminando la dependencia del token

--- src/tests/features/tasks/actions/delete-task-by-api.test.ts
+++ src/tests/features/tasks/actions/delete-task-by-api.test.ts
@@ -11,11 +11,8 @@ vi.mock("@/services/api", () => ({
 }));
 
 describe("taskApi", () => {
-	test("should first", async () => {
-		localStorage.setItem("task_token", "test_token");
-
+	test("should delete task whe delete task is success", async () => {
 		const taskId = 1;
-		const token = localStorage.getItem("task_token") as string;
 
 		vi.mocked(taskApi.delete).mockResolvedValue({
 			data: {
@@ -23,26 +20,20 @@ describe("taskApi", () => {
 			},
 		});
 
-		const result = await deleteTaskByApi(taskId, token);
+		const result = await deleteTaskByApi(taskId);
 
 		expect(result).toBe(true);
 		expect(taskApi.delete).toHaveBeenCalledTimes(1);
-		expect(taskApi.delete).toHaveBeenCalledWith(`/tasks/${taskId}`, {
-			headers: {
-				Authorization: `Bearer ${token}`,
-			},
-		});
 	});
 
 	test("should return false when delete task is fail", async () => {
-		localStorage.setItem("task_token", "test_token");
 		const taskId: number = 1;
-		const token: string = localStorage.getItem("task_token") as string;
 		const consoleSpy = vi.spyOn(console, "error").mockImplementation(() => {});
+
 		vi.mocked(taskApi.delete).mockRejectedValue(
 			new Error("Error deleting task"),
 		);
-		const result = await deleteTaskByApi(taskId, token);
+		const result = await deleteTaskByApi(taskId);
 
 		expect(result).toBe(false);
 	});
