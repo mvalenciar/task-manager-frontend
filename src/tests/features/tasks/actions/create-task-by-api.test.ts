542d110dc969463624a64197c46211eb7f48fd17
Milton Valencia R <miltonalonsovalenciarincon@gmail.com>
2026-08-07 17:40:16 -0500

    test(tasks): corregir pruebas unitarias de la acción createTaskByApi para validar el flujo sin tokens manuales

--- src/tests/features/tasks/actions/create-task-by-api.test.ts
+++ src/tests/features/tasks/actions/create-task-by-api.test.ts
@@ -10,45 +10,30 @@ vi.mock("@/services/api", () => ({
 
 describe("create-task-by-api", () => {
 	test("should create a task successfully when create-task is called", async () => {
-		localStorage.setItem("task_token", "test_token");
-
 		vi.mocked(taskApi.post).mockResolvedValue({
 			data: {
 				message: "✅ Tarea creada con éxito!",
 			},
 		});
-		const result = await createTaskByApi(
-			"test_token",
-			"Task_1",
-			"Description_1",
-		);
+		const result = await createTaskByApi("Task_1", "Description_1");
 
 		expect(result).toBe(true);
 		expect(taskApi.post).toHaveBeenCalledTimes(1);
-		expect(taskApi.post).toHaveBeenCalledWith(
-			"/tasks",
-			{
-				title: "Task_1",
-				description: "Description_1",
-			},
-			{
-				headers: {
-					Authorization: `Bearer test_token`,
-				},
-			},
-		);
+		expect(taskApi.post).toHaveBeenCalledWith("/tasks", {
+			title: "Task_1",
+			description: "Description_1",
+		});
 	});
 
 	test("should return false when the api is fail", async () => {
-		localStorage.setItem("task_token", "test_token");
 		const title = "Task_1";
 		const description = "Description_1";
-		const token = localStorage.getItem("task_token") as string;
+
 		const consoleSpy = vi.spyOn(console, "error").mockImplementation(() => {});
 
 		vi.mocked(taskApi.post).mockRejectedValue(new Error("Error creating task"));
 
-		const result = await createTaskByApi(token, title, description);
+		const result = await createTaskByApi(title, description);
 		expect(result).toBe(false);
 		consoleSpy.mockRestore();
 	});
