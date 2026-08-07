b3a982b07cc8861887e30d0a354bb53a777dd1f8
Milton Valencia R <miltonalonsovalenciarincon@gmail.com>
2026-08-07 11:06:04 -0500

    test(tasks): implementar pruebas unitarias para la acción toggleTaskByApi cubriendo estados de éxito y error

--- src/tests/features/tasks/actions/toggle-task-by-api.test.ts
+++ src/tests/features/tasks/actions/toggle-task-by-api.test.ts
@@ -16,38 +16,25 @@ describe("toggle-task-by-api", () => {
 			},
 		});
 
-		localStorage.setItem("task_token", "test_token");
 		const taskId = 1;
-		const token = localStorage.getItem("task_token") as string;
 		const completed = false;
-
-		const result = await toggleTaskByApi(taskId, completed, token);
+		const result = await toggleTaskByApi(taskId, completed);
 
 		expect(result).toBe(true);
 		expect(taskApi.put).toHaveBeenCalledTimes(1);
-		expect(taskApi.put).toHaveBeenCalledWith(
-			`/tasks/${taskId}`,
-			{
-				completed: !completed,
-			},
-			{
-				headers: {
-					Authorization: `Bearer ${token}`,
-				},
-			},
-		);
+		expect(taskApi.put).toHaveBeenCalledWith(`/tasks/${taskId}`, {
+			completed: !completed,
+		});
 	});
 
 	test("should fail toggle-task when api is fail", async () => {
-		localStorage.setItem("task_token", "test_token");
 		const taskId = 1;
-		const token = localStorage.getItem("task_token") as string;
 		const completed = false;
 		const consoleSpy = vi.spyOn(console, "error").mockImplementation(() => {});
 
 		vi.mocked(taskApi.put).mockRejectedValue(new Error("Fail updating task"));
 
-		const result = await toggleTaskByApi(taskId, completed, token);
+		const result = await toggleTaskByApi(taskId, completed);
 
 		expect(result).toBe(false);
 	});
